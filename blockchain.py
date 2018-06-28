28b6b37afcc03f8926bf27216eda7a2ca4c51f2d
Krishnanunni <krishnanunnir97@gmail.com>
2018-06-28 20:06:44 +0530

    Added api endpoints and fixed minor bugs

--- blockchain.py
+++ blockchain.py
@@ -1,15 +1,18 @@
 import json
 from time import time
 import hashlib
-def blockchain:
-    def __init__:
+from uuid import uuid4
+from flask import Flask, jsonify, request
+app=Flask(__name__)
+class Blockchain:
+    def __init__(self):
         self.chain=[]
         self.current_transactions=[]
-        self.new_block()
+        self.new_block(previous_hash=1,proof=100)
 
-    def new_block(self,previous_hash=None,proof):
+    def new_block(self,proof,previous_hash=None):
         block={
-            'index': len(self.chain)+1,
+            'index': len(self.chain) + 1,
             'time':time(),
             'transactions': self.current_transactions,
             'previous_hash': previous_hash,
@@ -28,7 +31,7 @@ def blockchain:
         return transaction
 
     @property
-    def last_block(property):
+    def last_block(self):
         return self.chain[-1]
     @staticmethod
     def hash(block):
@@ -37,10 +40,50 @@ def blockchain:
 
     def proof_of_work(self,last_proof):
         proof=0
-        if(not valid_proof(proof,last_proof)){
+        if(not self.valid_proof(proof,last_proof)):
             proof+=1;
-        }
-
+        return proof
+    @staticmethod
     def valid_proof(proof,last_proof):
         guess_hash=f'{proof}{last_proof}'
-        return hashlib.sha256(guess_hash).hex_digest[:4]==='0000'
+        return hashlib.sha256(guess_hash).hex_digest()[:4]=='0000'
+
+node_identifier = str(uuid4()).replace('-', '')
+blockchain = Blockchain()
+@app.route('/transaction/new',methods=['POST'])
+def transaction():
+    unit = request.get_json()
+    req_values=['sender','reciever','data']
+    if not all(i in unit for i in req_values):
+        return "Missing Values",400
+    index=blockchain.new_transaction(unit['sender'],unit['reciever'],unit['amount'])
+    response={'message':f'Transaction will be added to the block {index}'}
+    return jsonify(response),200
+
+@app.route('/mine/',methods=['GET'])
+def mine():
+    last_blockchain = blockchain.last_block
+    last_proof = last_blockchain['proof']
+    proof = blockchain.proof_of_work(last_proof)
+    previous_hash=blockchain.hash(last_blockchain)
+    block = blockchain.new_block(proof,previous_hash)
+    blockchain.new_transactions(sender="0",receiver=node_identifier,amount=1)
+    response = {
+        'index':block['index'],
+        'time':time(),
+        'transactions':block['current_transactions'],
+        'previous_hash':block['previous_hash'],
+        'proof':proof,
+    }
+    return jsonify(response),200;
+
+@app.route('/chain',methods=['GET'])
+def chain():
+    response = {
+        'chain':blockchain.chain,
+        'length':len(blockchain.chain),
+    }
+    return jsonify(response),200
+
+if __name__=="__main__":
+    app.run('0.0.0.0',port=5000)
